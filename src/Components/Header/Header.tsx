1451d24afacc4bd72e4e708f4620f8a77c473e17
Astaroth <minatosavich@gmail.com>
2021-07-14 21:55:02 +0300

    fix: header refactor media

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import s from './Header.module.css';
 import {Navigation} from "./Navigation/Navigation";
+import sContainer from './../../common/Styles/Container.module.css'
+
 
 export const Header = () => {
     return (
-        <header className={s.header}>
-            <Navigation/>
-        </header>
+            <header className={`${s.header}`}>
+                <Navigation/>
+            </header>
     );
 }
